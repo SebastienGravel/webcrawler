88ec681ba498c5580d799d6914a3b6ac8a9b9a1c
Sebastien Gravel <seb@polyaxis.xyz>
2022-04-10 03:23:32 -0400

    New date function

--- Articles.py
+++ Articles.py
@@ -136,6 +136,67 @@ class Articles:
                 img_filename = article_img.split("/")[-1]  
             return [article_img, img_filename]
         """
+    
+    def date_mdy(self, soup):
+        alphabet_soup = soup
+        datex = re.findall("[A-Za-z]+\s[0-9]+,?\s[0-9]+", alphabet_soup)
+        return datex
+
+    def date_dmy(self, soup):
+        alphabet_soup = soup
+        datex = re.findall("[0-9]+\s[A-Za-z]+,?\s[0-9]+", alphabet_soup)
+        return datex
+
+    def date_slash(self, soup):
+        alphabet_soup = soup
+        datex = re.findall("[0-9]+\\/[0-9]+\\/[0-9]+", alphabet_soup)
+        return datex
+
+    def date_dash(self, soup):
+        alphabet_soup = soup
+        datex = re.findall("[0-9]+-[0-9]+-[0-9]+", alphabet_soup)
+        return datex
+
+
+    def extract_date(self,date_val,type,month_dict):
+        fulltime = str(date_val)
+        fulltime = fulltime.replace(",","")
+        array_date = fulltime.split(" ")
+
+        if type == 0:
+            month_value = array_date[0].lower()
+            if month_value.isalpha():
+                for m,n in month_dict.items():
+                    #if month_value == m:
+                    if m.find(month_value) != -1:
+                        month = n
+            else:
+                month = month_value
+            
+            days_format = int(array_date[1]) if int(array_date[1]) > 10 else int('00')+int(array_date[1])
+            published_date = array_date[2]+"-"+month+"-"+str(days_format)
+        elif type == 1:
+            month_value = array_date[1].lower()
+            if month_value.isalpha():
+                for m,n in month_dict.items():
+                    #if month_value == m:
+                    if m.find(month_value) != -1:
+                        month = n
+            else:
+                month = month_value
+                
+            days_format = int(array_date[0]) if int(array_date[0]) > 10 else int('00')+int(array_date[0])
+            published_date = array_date[2]+"-"+month+"-"+str(days_format)
+        elif type == 2:
+            dash_date_array = date_val[0].split("/")
+            published_date = dash_date_array[2]+"-"+dash_date_array[1]+"-"+dash_date_array[0] 
+        elif type == 3:
+            published_date = str(date_val[0])
+        else:
+            pass
+
+        return published_date
+
 
     def date(self, soup):
         month_dict = {
@@ -156,16 +217,15 @@ class Articles:
         published_date = "0000-00-00"
         
         try:
-            if soup.find('article').find("time"):
-                #print("Time tag exist")
-                time = soup.find('article').find('time')
+            if soup.find("article").find("time"):
+                #print("Time in article tag exist")
+                time = soup.find("article").find("time")
                 for k,v in time.attrs.items():
                     if k == "datetime":
                         fulltime = time['datetime'].split("T")
                         article_date = fulltime[0].split("-")
                         published_date = article_date[0]+"-"+article_date[1]+"-"+article_date[2]
                         break
-                        print(published_date) 
                     elif k == "content":
                         special_characters = [',','/','.','-']
                         fulltime = time['content'].strip()
@@ -188,66 +248,145 @@ class Articles:
                     else:
                         #print(time.attrs)
                         published_date = "0000-00-00"
+                #print(published_date)
+                return published_date
 
+            elif soup.find("time"):
+                #print("Time tag exist")
+                time = soup.find("time")
+                for k,v in time.attrs.items():
+                    if k == "datetime":
+                        fulltime = time['datetime'].split("T")
+                        article_date = fulltime[0].split("-")
+                        published_date = article_date[0]+"-"+article_date[1]+"-"+article_date[2]
+                        break
+                    elif k == "content":
+                        special_characters = [',','/','.','-']
+                        fulltime = time['content'].strip()
+                        fulltime = ''.join(filter(lambda i:i not in special_characters, fulltime))
+                        fulltime = fulltime.split(" ")
+
+                        month_value = fulltime[0].lower()
+
+                        if month_value.isalpha():
+                            for m,n in month_dict.items():
+                                #if month_value == m:
+                                if m.find(month_value) != -1:
+                                    month = n
+                        else:
+                            month = month_value
+
+                        #article_date = [fulltime[2], month, fulltime[1]]
+                        published_date = fulltime[2]+"-"+month+"-"+fulltime[1]
+                        break
+                    else:
+                        #print(time.attrs)
+                        published_date = "0000-00-00"
+                #print(published_date)
+                return published_date
             else:
-                search_date = soup.find('article').findAll(text=re.compile('[A-Za-z]+\s[0-9]+,?\s[0-9]+')) # Aaa+ 00, 0000
-                rev_text_date = soup.find('article').findAll(text=re.compile('[0-9]+\s[A-Za-z]+,?\s[0-9]+')) # 00 Aaa+, 0000
-                dash_date = soup.find('article').findAll(text=re.compile('[0-9]+\\/[0-9]+\\/[0-9]+')) # 00/00/0000
-                
-                """
-                print("Regex time")
-                print(search_date)
-                print(rev_text_date)
-                print(dash_date)
-                """
 
+                soup_date = str(soup.find("body"))
+
+                search_date = self.date_mdy(soup_date) # Aaa+ 00, 0000
+                rev_text_date = self.date_dmy(soup_date) # 00 Aaa+, 0000
+                slash_date = self.date_slash(soup_date) # 00/00/0000
+                dash_date = self.date_dash(soup_date) # 00-00-0000
+
+                """
+                print(search_date[0])
+                sd = self.extract_date(search_date[0], 0, month_dict)
+                print("Search "+ sd)
+
+                print(rev_text_date[0])
+                rd = self.extract_date(rev_text_date[0], 1, month_dict)
+                print("rd "+ rd)
+
+                print(slash_date[0])
+                sd = self.extract_date(slash_date[0], 2, month_dict)
+                print("sd "+ sd)
+
+                print(dash_date[0])
+                dd = self.extract_date(dash_date[0], 3, month_dict)
+                print("Dash "+ dd)
+
+                """
                 if len(search_date) > 0:
-                    fulltime = search_date[0]
-                    fulltime = fulltime.replace(",","")
-                    array_date = fulltime.split(" ")
-
-                    month_value = array_date[0].lower()
-
-                    if month_value.isalpha():
-                        for m,n in month_dict.items():
-                            #if month_value == m:
-                            if m.find(month_value) != -1:
-                                month = n
-                    else:
-                        month = month_value
-
-                    published_date = array_date[2]+"-"+month+"-"+array_date[1]
-                    #print(published_date)
+                    published_date = self.extract_date(search_date[0], 0, month_dict)
+                    #print("Pub "+published_date)
                 elif len(rev_text_date) > 0:
-                    fulltime = rev_text_date[0]
-                    fulltime = fulltime.replace(",","")
-                    array_date = fulltime.split(" ")
-
-                    month_value = array_date[1].lower()
-
-                    if month_value.isalpha():
-                        for m,n in month_dict.items():
-                            #if month_value == m:
-                            if m.find(month_value) != -1:
-                                month = n
-                    else:
-                        month = month_value
-
-                    published_date = array_date[2]+"-"+month+"-"+array_date[0]
-                    #print(published_date)
+                    published_date = self.extract_date(rev_text_date[0], 1, month_dict)
+                    #print("Pub "+published_date)
+                elif len(slash_date) > 0:
+                    published_date = self.extract_date(slash_date[0], 2, month_dict) 
+                    #print("Pub "+published_date)
                 elif len(dash_date) > 0:
-                    dash_date_array = dash_date.split("/")
-                    published_date = dash_date_array[2]+"-"+dash_date_array[1]+"-"+dash_date_array[0] 
-                    #print(published_date)
+                    published_date = self.extract_date(dash_date[0], 3, month_dict)
+                    #print("Pub "+published_date)
                 else:
-                    pass
-
-        except:
+                    published_date = "0000-00-00"
+                    #print(published_date)
+                
+                
+        except Exception as e:
             #print("No date available")
-            published_date = "0000-00-00"
-
+            #print(e)
+            #published_date = "0000-00-00"
+            pass
+        
         return published_date
 
-    def url():
-        pass
+    def regextime(self, soup, month_dict, mode):
+
+        if mode == 'article':
+            search_date = soup.find('article').findAll(text=re.compile('[A-Za-z]+\s[0-9]+,?\s[0-9]+')) # Aaa+ 00, 0000
+            rev_text_date = soup.find('article').findAll(text=re.compile('[0-9]+\s[A-Za-z]+,?\s[0-9]+')) # 00 Aaa+, 0000
+            dash_date = soup.find('article').findAll(text=re.compile('[0-9]+\\/[0-9]+\\/[0-9]+')) # 00/00/0000
+        else:
+            search_date = soup.findAll(text=re.compile('[A-Za-z]+\s[0-9]+,?\s[0-9]+')) # Aaa+ 00, 0000
+            rev_text_date = soup.findAll(text=re.compile('[0-9]+\s[A-Za-z]+,?\s[0-9]+')) # 00 Aaa+, 0000
+            dash_date = soup.findAll(text=re.compile('[0-9]+\\/[0-9]+\\/[0-9]+')) # 00/00/0000
+
+        if len(search_date) > 0:
+            fulltime = search_date[0]
+            fulltime = fulltime.replace(",","")
+            array_date = fulltime.split(" ")
+            month_value = array_date[0].lower()
+
+            if month_value.isalpha():
+                for m,n in month_dict.items():
+                    #if month_value == m:
+                    if m.find(month_value) != -1:
+                        month = n
+            else:
+                month = month_value
+
+            published_date = array_date[2]+"-"+month+"-"+array_date[1]
+            #print(published_date)
+        elif len(rev_text_date) > 0:
+            fulltime = rev_text_date[0]
+            fulltime = fulltime.replace(",","")
+            array_date = fulltime.split(" ")
+            month_value = array_date[1].lower()
+
+            if month_value.isalpha():
+                for m,n in month_dict.items():
+                    #if month_value == m:
+                    if m.find(month_value) != -1:
+                        month = n
+            else:
+                month = month_value
+
+            published_date = array_date[2]+"-"+month+"-"+array_date[0]
+            #print(published_date)
+        elif len(dash_date) > 0:
+            dash_date_array = dash_date.split("/")
+            published_date = dash_date_array[2]+"-"+dash_date_array[1]+"-"+dash_date_array[0] 
+            #print(published_date)
+        else:
+            pass
+
+        return published_date
     
+    
+        
