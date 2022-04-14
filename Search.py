0f5f59c1b3a7f72d86979ae9ea073160755cf8a0
Sebastien Gravel <seb@polyaxis.xyz>
2022-04-14 03:46:34 -0400

    Add a conditions to ignore videos search results

--- Search.py
+++ Search.py
@@ -4,20 +4,32 @@ import requests
 
 class Search:
 
-    def __init__(self, site, subject, mode):
+    def __init__(self, site, subject, mode, allsite):
         self.site = site
         self.subject = subject
         self.mode = mode
+        self.allsite = allsite
 
     def get_results(self):
         urls = []
         #search_url = "https://ca.search.yahoo.com/search;_ylt=AwrJ7KMAVrhg8V8AVHvrFAx.;_ylc=X1MDMjExNDcyMTAwMwRfcgMyBGZyA3lmcC10BGdwcmlkA3BMM0VLOEtFVHNpUVVESU5waUh5eUEEbl9yc2x0AzAEbl9zdWdnAzIEb3JpZ2luA2NhLnNlYXJjaC55YWhvby5jb20EcG9zAzAEcHFzdHIDBHBxc3RybAMwBHFzdHJsAzY2BHF1ZXJ5A3NpdGUlM0FpbnRlcmVzdGluZ2VuZ2luZWVyaW5nLmNvbSUyMCUyMmhlaXNlbmJlcmclMjB1bmNlcnRhaW50eSUyMHByaW5jaXBsZSUyMgR0X3N0bXADMTYyMjY5MzQyNA--?p=site%3A"+self.site+"+%22"+self.subject+"%22&fr2=sb-top&fr=yfp-t&fp=1"
         
-        if(self.mode == 1):
+        if self.mode == 1:
             subject_term = self.subject.replace(" ","+")
-            search_url = "https://ca.search.yahoo.com/search;_ylt=AwrJ7KMAVrhg8V8AVHvrFAx.;_ylc=X1MDMjExNDcyMTAwMwRfcgMyBGZyA3lmcC10BGdwcmlkA3BMM0VLOEtFVHNpUVVESU5waUh5eUEEbl9yc2x0AzAEbl9zdWdnAzIEb3JpZ2luA2NhLnNlYXJjaC55YWhvby5jb20EcG9zAzAEcHFzdHIDBHBxc3RybAMwBHFzdHJsAzY2BHF1ZXJ5A3NpdGUlM0FpbnRlcmVzdGluZ2VuZ2luZWVyaW5nLmNvbSUyMCUyMmhlaXNlbmJlcmclMjB1bmNlcnRhaW50eSUyMHByaW5jaXBsZSUyMgR0X3N0bXADMTYyMjY5MzQyNA--?p=site%3A"+self.site+"+"+subject_term+"&fr2=sb-top&fr=yfp-t&fp=1"
+            if self.allsite == 1:
+                search_url = "https://ca.search.yahoo.com/search;_ylt=AwrJ7KMAVrhg8V8AVHvrFAx.;_ylc=X1MDMjExNDcyMTAwMwRfcgMyBGZyA3lmcC10BGdwcmlkA3BMM0VLOEtFVHNpUVVESU5waUh5eUEEbl9yc2x0AzAEbl9zdWdnAzIEb3JpZ2luA2NhLnNlYXJjaC55YWhvby5jb20EcG9zAzAEcHFzdHIDBHBxc3RybAMwBHFzdHJsAzY2BHF1ZXJ5A3NpdGUlM0FpbnRlcmVzdGluZ2VuZ2luZWVyaW5nLmNvbSUyMCUyMmhlaXNlbmJlcmclMjB1bmNlcnRhaW50eSUyMHByaW5jaXBsZSUyMgR0X3N0bXADMTYyMjY5MzQyNA--?p="+subject_term+"&fr2=sb-top&fr=yfp-t&fp=1"
+            else:
+                search_url = "https://ca.search.yahoo.com/search;_ylt=AwrJ7KMAVrhg8V8AVHvrFAx.;_ylc=X1MDMjExNDcyMTAwMwRfcgMyBGZyA3lmcC10BGdwcmlkA3BMM0VLOEtFVHNpUVVESU5waUh5eUEEbl9yc2x0AzAEbl9zdWdnAzIEb3JpZ2luA2NhLnNlYXJjaC55YWhvby5jb20EcG9zAzAEcHFzdHIDBHBxc3RybAMwBHFzdHJsAzY2BHF1ZXJ5A3NpdGUlM0FpbnRlcmVzdGluZ2VuZ2luZWVyaW5nLmNvbSUyMCUyMmhlaXNlbmJlcmclMjB1bmNlcnRhaW50eSUyMHByaW5jaXBsZSUyMgR0X3N0bXADMTYyMjY5MzQyNA--?p=site%3A"+self.site+"+"+subject_term+"&fr2=sb-top&fr=yfp-t&fp=1"
+                
+            #search_url = "https://ca.search.yahoo.com/search;_ylt=AwrJ7KMAVrhg8V8AVHvrFAx.;_ylc=X1MDMjExNDcyMTAwMwRfcgMyBGZyA3lmcC10BGdwcmlkA3BMM0VLOEtFVHNpUVVESU5waUh5eUEEbl9yc2x0AzAEbl9zdWdnAzIEb3JpZ2luA2NhLnNlYXJjaC55YWhvby5jb20EcG9zAzAEcHFzdHIDBHBxc3RybAMwBHFzdHJsAzY2BHF1ZXJ5A3NpdGUlM0FpbnRlcmVzdGluZ2VuZ2luZWVyaW5nLmNvbSUyMCUyMmhlaXNlbmJlcmclMjB1bmNlcnRhaW50eSUyMHByaW5jaXBsZSUyMgR0X3N0bXADMTYyMjY5MzQyNA--?p=site%3A"+self.site+"+"+subject_term+"&fr2=sb-top&fr=yfp-t&fp=1"
         else:
-            search_url = "https://ca.search.yahoo.com/search;_ylt=AwrJ7KMAVrhg8V8AVHvrFAx.;_ylc=X1MDMjExNDcyMTAwMwRfcgMyBGZyA3lmcC10BGdwcmlkA3BMM0VLOEtFVHNpUVVESU5waUh5eUEEbl9yc2x0AzAEbl9zdWdnAzIEb3JpZ2luA2NhLnNlYXJjaC55YWhvby5jb20EcG9zAzAEcHFzdHIDBHBxc3RybAMwBHFzdHJsAzY2BHF1ZXJ5A3NpdGUlM0FpbnRlcmVzdGluZ2VuZ2luZWVyaW5nLmNvbSUyMCUyMmhlaXNlbmJlcmclMjB1bmNlcnRhaW50eSUyMHByaW5jaXBsZSUyMgR0X3N0bXADMTYyMjY5MzQyNA--?p=site%3A"+self.site+"+%22"+self.subject+"%22&fr2=sb-top&fr=yfp-t&fp=1"
+            if self.allsite == 1:
+                search_url = "https://ca.search.yahoo.com/search;_ylt=AwrJ7KMAVrhg8V8AVHvrFAx.;_ylc=X1MDMjExNDcyMTAwMwRfcgMyBGZyA3lmcC10BGdwcmlkA3BMM0VLOEtFVHNpUVVESU5waUh5eUEEbl9yc2x0AzAEbl9zdWdnAzIEb3JpZ2luA2NhLnNlYXJjaC55YWhvby5jb20EcG9zAzAEcHFzdHIDBHBxc3RybAMwBHFzdHJsAzY2BHF1ZXJ5A3NpdGUlM0FpbnRlcmVzdGluZ2VuZ2luZWVyaW5nLmNvbSUyMCUyMmhlaXNlbmJlcmclMjB1bmNlcnRhaW50eSUyMHByaW5jaXBsZSUyMgR0X3N0bXADMTYyMjY5MzQyNA--?p=%22"+self.subject+"%22&fr2=sb-top&fr=yfp-t&fp=1"
+            else:
+                search_url = "https://ca.search.yahoo.com/search;_ylt=AwrJ7KMAVrhg8V8AVHvrFAx.;_ylc=X1MDMjExNDcyMTAwMwRfcgMyBGZyA3lmcC10BGdwcmlkA3BMM0VLOEtFVHNpUVVESU5waUh5eUEEbl9yc2x0AzAEbl9zdWdnAzIEb3JpZ2luA2NhLnNlYXJjaC55YWhvby5jb20EcG9zAzAEcHFzdHIDBHBxc3RybAMwBHFzdHJsAzY2BHF1ZXJ5A3NpdGUlM0FpbnRlcmVzdGluZ2VuZ2luZWVyaW5nLmNvbSUyMCUyMmhlaXNlbmJlcmclMjB1bmNlcnRhaW50eSUyMHByaW5jaXBsZSUyMgR0X3N0bXADMTYyMjY5MzQyNA--?p=site%3A"+self.site+"+%22"+self.subject+"%22&fr2=sb-top&fr=yfp-t&fp=1"
+                pass
+            
+            #search_url = "https://ca.search.yahoo.com/search;_ylt=AwrJ7KMAVrhg8V8AVHvrFAx.;_ylc=X1MDMjExNDcyMTAwMwRfcgMyBGZyA3lmcC10BGdwcmlkA3BMM0VLOEtFVHNpUVVESU5waUh5eUEEbl9yc2x0AzAEbl9zdWdnAzIEb3JpZ2luA2NhLnNlYXJjaC55YWhvby5jb20EcG9zAzAEcHFzdHIDBHBxc3RybAMwBHFzdHJsAzY2BHF1ZXJ5A3NpdGUlM0FpbnRlcmVzdGluZ2VuZ2luZWVyaW5nLmNvbSUyMCUyMmhlaXNlbmJlcmclMjB1bmNlcnRhaW50eSUyMHByaW5jaXBsZSUyMgR0X3N0bXADMTYyMjY5MzQyNA--?p=site%3A"+self.site+"+%22"+self.subject+"%22&fr2=sb-top&fr=yfp-t&fp=1"
         
         website = requests.get(search_url)
         soup = BeautifulSoup(website.content, 'html.parser')
@@ -33,7 +45,8 @@ class Search:
                         if a['href'].find("images.") == -1:
                             if a['href'].find(".pdf") == -1:
                                 if a['href'].find(".stm") == -1:
-                                    urls.append(a['href'])
+                                        if a['href'].find(".video") == -1:
+                                            urls.append(a['href'])
         return urls
 
 
