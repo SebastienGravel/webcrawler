e60141b6878610264786d1ffb4630e1c950b214a
Sebastien Gravel <seb@polyaxis.xyz>
2022-04-15 13:44:59 -0400

    Clean the code

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from ast import arg
 from Search import Search
 from Articles import Articles
 from termcolor import colored
-import requests, sys, shutil, json, time, datetime, pathlib, os, argparse, re
+import requests, sys, shutil, json, time, datetime, pathlib, os, argparse
 
 def get_time():
     now = datetime.datetime.now()
@@ -247,7 +247,7 @@ else:
                                 os.mkdir(path_term)
 
                             link = r.split("/")
-                            print(link)
+                            #print(link)
                             site_name = link[2].replace("www.","")
                             path_site = os.path.join(path_term, site_name)        
                             if not os.path.exists(path_site):
@@ -322,109 +322,6 @@ else:
         else:
             print(colored("No result found for {} on website {}".format(s,site_name),"red"))
 
-
-
-
-"""
-for s in subject:
-    for w in website:
-        look_up = Search(w,s, args.nostrict, args.allsite)
-        result = look_up.get_results()
-
-        if result:
-            for r in result:
-                if not is_article_exist(r, existing_articles):
-                    try:
-                        page = Articles(r)
-                        article = page.article()
-                        header = page.title(article).strip()
-                        text = page.text(article).strip()
-                        img = page.image(article,r)
-                        article_date = page.date(article)
-
-                        if article_date == "0000-00-00":
-                            debug_date = debug_date + 1
-
-                        header_count = len(header)
-                        article_count = len(article)
-                        text_count = len(text)
-                        save_search = args.debug
-
-                        if header_count != 0 and text_count > 200 and len(img) > 0 and save_search == None:
-
-                            folder_term = s.replace(" ","_")
-                            path_term = os.path.join(path,folder_term)
-                            title_folder = clean_title(header)
-
-                            if not os.path.exists(path_term):
-                                #os.makedirs('my_folder')
-                                os.mkdir(path_term)
-
-                            path_site = os.path.join(path_term, w)        
-                            if not os.path.exists(path_site):
-                                #os.makedirs('my_folder')
-                                os.mkdir(path_site)
-
-                            #path_article = os.path.join(path_site,get_time())
-                            path_article = os.path.join(path_site,title_folder)
-                            os.mkdir(path_article)
-                            #os.mkdir(path_article+'/Bilder')
-
-                            if header:
-                                #with open(path_article+'/Bilder/'+'Headline.txt', 'w') as f:
-                                with open(path_article+'/Headline.txt', 'w') as f:
-                                    f.write(header)
-                                time.sleep(.7)
-                                        
-                            if text:
-                                text = text.strip()
-                                #with open(path_article+'/Bilder/'+'Text.txt', 'w') as f:
-                                with open(path_article+'/Text.txt', 'w') as f:
-                                    f.write(text)
-                                time.sleep(.7)
-                            
-                            if len(text) != 0:
-                                with open(path_article+'/Lenght.txt', 'w') as f:
-                                    f.write(str(text_count))
-                                time.sleep(.7)
-                            
-                            if r:
-                                #with open(path_article+'/Bilder/'+'Link.txt', 'w') as f:
-                                with open(path_article+'/Link.txt', 'w') as f:
-                                    f.write(r)
-                                time.sleep(.7)
-                            
-                            if article_date:
-                                #with open(path_article+'/Bilder/'+'Link.txt', 'w') as f:
-                                with open(path_article+'/'+article_date+'.txt', 'w') as f:
-                                    f.write(article_date)
-                                time.sleep(.7)
-                            
-                            # multiple img
-                            if img:
-                                os.mkdir(path_article+'/img')
-                                imgt = len(img)
-                                for c,i in enumerate(img):
-                                    path_img = path_article+'/img/'+i[1] 
-                                    save_img(i[0], path_img)           
-                                    #print(colored("Saving img{} of {} ".format(c,imgt),"green"))
-
-
-                            console_output(w,s,header,text_count,img,article_date)
-                            save_article_title(r)
-                            time.sleep(.7)
-                        elif header_count != 0 and text_count > 200 and len(img) > 0 and save_search == 1:
-                            console_output(w,s,header,text_count,img,article_date)
-                            time.sleep(.7)
-
-                        
-                    except Exception as e:
-                        console_reject(r)
-                else:
-                    print(colored("Article already exist, skipping ...","red"))
-        else:
-            print(colored("No result found for {} on website {}".format(s,w),"red"))
-"""
 
 end = datetime.datetime.now() - start       
 print(colored("Web search is over!","green"))
